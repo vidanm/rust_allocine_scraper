899382994f89579e187fc8fa61a7fc0637c25f13
vidanm <vidan.tauriff@gmail.com>
2024-03-08 22:02:38 +0100

    Error handling

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,32 @@
+pub mod errors;
 pub mod movie;
 mod tests;
+use errors::AppError;
 use fantoccini::elements::Element;
 use fantoccini::error::CmdError;
 use fantoccini::{Client, ClientBuilder, Locator};
+use movie::screening::Screening;
 use movie::Movie;
-use std::io;
 use unidecode::unidecode;
 
-async fn accept_paywall(p_client: &Client) -> Result<(), CmdError> {
+type Result<T> = std::result::Result<T, AppError>;
+
+async fn accept_paywall(p_client: &Client) -> Result<()> {
     //Click the paywall if there is one
     p_client.goto("https://www.allocine.fr").await?;
     let v_button = p_client
         .find(Locator::Css("button.jad_cmp_paywall_button-cookies"))
         .await;
 
-    match v_button {
-        Ok(i) => i.click().await,
-        Err(_) => Ok(()), // Skip if there is no paywall
+
+    if let Ok(i) = v_button {
+        i.click().await?
     }
+
+    Ok(()) //Skip if there is no paywall
 }
 
-async fn get_all_cards_of_search(
-    p_client: &Client,
-    p_movie: &Movie,
-) -> Result<Vec<Element>, CmdError> {
+async fn get_all_cards_of_search(p_client: &Client, p_movie: &Movie) -> Result<Vec<Element>> {
     //Find all movie cards in the search page
     p_client.goto(&p_movie.search_url).await?;
     p_client.current_url().await?;
@@ -33,10 +36,7 @@ async fn get_all_cards_of_search(
     Ok(cards)
 }
 
-async fn get_movie_card(
-    p_movie: &Movie,
-    p_cards: &Vec<Element>,
-) -> Result<Option<Element>, CmdError> {
+async fn get_movie_card(p_movie: &Movie, p_cards: &Vec<Element>) -> Result<Element> {
     let card_iter = p_cards.into_iter();
     for card in card_iter {
         let director = unidecode(
@@ -59,31 +59,96 @@ async fn get_movie_card(
         )
         .to_ascii_uppercase();
 
-        println!("{} {}",&movie_title,&director);
+        // println!("{} {}", &movie_title, &director);
 
-        let card_of_movie: Option<Element> = match director.as_str() {
-            ref x if x == &p_movie.director => match movie_title.as_str() {
-                ref y if y == &p_movie.title => Some(card.clone()), //todo : clone nécessaire ?
-                _ => None,
-            },
-            _ => None,
-        };
-
-        return Ok(card_of_movie);
+        if movie_title == p_movie.title && director == p_movie.director {
+            return Ok(card.clone());
+        }
     }
-    Err(CmdError::InvalidArgument(
-        String::from(""),
-        String::from(""),
-    )) //todo
+
+    Err(AppError::ElementNotFound)
 }
 
-async fn go_to_movie_page(p_card: &Element) -> Result<(), CmdError> {
-    p_card.click().await?;
+async fn go_to_screening_page(p_client: &Client, p_card: &Element) -> Result<()> {
+    let link_to_movie_page = p_card.find(Locator::Css("a.meta-title-link")).await?;
+
+    link_to_movie_page.click().await?;
+
+    let screening_tab = p_client
+        .find(Locator::Css("#js-showtimes-nav-redir"))
+        .await?;
+
+    screening_tab.click().await?;
+    Ok(())
+}
+
+async fn get_date_tabs(p_client: &Client) -> Result<Vec<Element>> {
+
+    let date_tabs = p_client
+        .find_all(Locator::Css("span.calendar-date-link:not(.disabled)"))
+        .await?;
+
+    Ok(date_tabs)
+}
+
+async fn get_theater_cards(p_date_tab: &Element) -> Result<Vec<Element>> {
+    println!("B");
+
+    p_date_tab.click().await?;
+    let result = p_date_tab
+        .find_all(Locator::Css("div.theater-card"))
+        .await?;
+    println!("B");
+
+    Ok(result)
+}
+
+async fn get_theater_screenings(p_theater_card: &Element) -> Result<Vec<Screening>> {
+    let mut screenings: Vec<Screening> = Vec::new();
+
+    println!("A");
+
+    let v_screening_cards = p_theater_card
+        .find_all(Locator::Css("span.showtimes-hour-item-value"))
+        .await?;
+
+        println!("A");
+
+    for scr_card in v_screening_cards.into_iter() {
+        screenings.push(Screening::new(
+            scr_card.prop("text").await?.unwrap(),
+            String::from("UNDEFINED"),
+            String::from("UNDEFINED"),
+        ));
+    }
+    println!("A");
+    println!("{:?}", screenings);
+    Ok(screenings)
+}
+
+async fn do_steps(p_client: &Client) -> Result<()> {
+    let test_movie = Movie::new(String::from("Dune"), String::from("Denis Villeneuve"));
+
+    let cards = get_all_cards_of_search(p_client, &test_movie).await?;
+    let movie_card = get_movie_card(&test_movie, &cards).await?;
+
+    go_to_screening_page(p_client, &movie_card).await?;
+
+    let date_tabs: Vec<Element> = get_date_tabs(&p_client).await?;
+    let date_tab = date_tabs.first().ok_or_else(|| AppError::ElementNotFound)?;
+    let theater_cards: Vec<Element> = get_theater_cards(&date_tab).await?;
+
+    let theater_card = theater_cards
+        .first()
+        .ok_or_else(|| AppError::ElementNotFound)?;
+
+    let _theater_screenings = get_theater_screenings(&theater_card).await?;
+
     Ok(())
 }
 
 #[tokio::main]
-async fn main() -> Result<(), CmdError> {
+async fn main() -> Result<()> {
     let client = ClientBuilder::native()
         .connect("http://localhost:4444")
         .await
@@ -91,32 +156,28 @@ async fn main() -> Result<(), CmdError> {
             "Failed to connect to WebDriver. \n 
             Start the WebDriver with 'geckodriver' command",
         );
-    accept_paywall(&client).await?;
 
-    // loop {
-        let mut input_movie = String::new();
-        println!("Entrer le nom du film : ");
-        io::stdin().read_line(&mut input_movie).expect("Failed to read line");
-        input_movie = String::from(input_movie.trim_end());
+    //todo : add cookie for paywall ?
 
-        let mut input_director = String::new();
-        println!("Entrer le nom du réalisateur: ");
-        io::stdin().read_line(&mut input_director).expect("Failed to read line");
-        input_director = String::from(input_director.trim_end());
+    //S'occupe d'aller sur les pages et de cliquer sur le paywall quand il se présente
+    match do_steps(&client).await {
+        Ok(c) => c,
+        Err(e) => match e {
+            AppError::IllegalArgument => println!("Illegal Argument Error"),
+            AppError::ElementNotFound => println!("Element Not Found Error"),
+            AppError::CmdError(e) => match e {
+                CmdError::Standard(wd) => match wd.error() {
+                    "element click intercepted" => {
+                        let _ = accept_paywall(&client).await;
+                        let _ = do_steps(&client).await;
+                    },
+                    _s => println!("{}",_s) //todo Revoir
+                },
+                _e => println!("{}",_e) //todo Revoir
+            },
+        },
+    };
 
-
-        let test_movie = Movie::new(input_movie,input_director);
-
-        //Gérer l'erreur ?
-        let cards = get_all_cards_of_search(&client, &test_movie).await?;
-        let movie_card = get_movie_card(&test_movie, &cards).await?;
-        match movie_card {
-            Some(i) => go_to_movie_page(&i).await?,
-            _ => println!(
-                "No card found for this movie : {} {}",
-                test_movie.title, test_movie.director
-            ),
-        // };
-    }
-    client.close().await
+    let out = client.close().await?;
+    Ok(out)
 }
--- src/movie/mod.rs
+++ src/movie/mod.rs
@@ -1,12 +1,23 @@
 pub mod screening;
 use screening::Screening;
 use unidecode::unidecode;
+use core::fmt::{Result,Formatter,Display};
 
-
+#[derive(Debug)]
 pub struct Movie {
     pub search_url : String,
     pub title: String,
     pub director: String,
+}
+
+impl Display for Movie {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        // Write strictly the first element into the supplied output
+        // stream: `f`. Returns `fmt::Result` which indicates whether the
+        // operation succeeded or failed. Note that `write!` uses syntax which
+        // is very similar to `println!`.
+        write!(f, "{} : {}", self.title, self.director)
+    }
 }
 
 impl Movie {
@@ -20,49 +31,6 @@ impl Movie {
             director: unidecode(&director).to_ascii_uppercase(),
         }
     }
-
-    // fn get_movie_url(title: &String, _direction: &String) -> Option<String> {
-    //     let url_start: String = String::from("https://www.allocine.fr/rechercher/?q=");
-    //     let url = format!("{}{}", url_start, title);
-    //     let response = reqwest::blocking::get(url).unwrap().text().unwrap();
-    //     let document = Html::parse_document(&response);
-    //     let card_selector = Selector::parse("div.entity-card>.meta").unwrap();
-    //     let direction_selector = Selector::parse("div.meta-body-direction>span").unwrap();
-    //     let title_selector = Selector::parse("h2.meta-title>span").unwrap();
-    //     let link_selector = Selector::parse("h2.meta-title>span").unwrap();
-
-    //     //todo itérer sur toutes les cards
-    //     // et comparer les noms real et titres
-    //     let card = document.select(&card_selector).next().unwrap();
-    //     let found_title: String = card
-    //         .select(&title_selector)
-    //         .map(|x| x.html())
-    //         .collect();
-
-    //     let found_direction: String = card
-    //         .select(&direction_selector)
-    //         .map(|x| unidecode(&x.inner_html()).to_ascii_uppercase())
-    //         .filter(|x| !x.eq("DE"))
-    //         .collect();
-
-    //     // let found_url: String = card
-    //     //     .select(&link_selector)
-    //     //     .map(|x| x.value().attr("href").unwrap())
-    //     //     .collect();
-    //     let found_url = String::from("");
-    //     for el in card.select(&link_selector) {
-    //         let href = el.value().attr("href");
-    //         if href != None {
-    //             println!("a {}",href.unwrap());
-    //         }
-    //     }
-
-    //     println!(
-    //         "Trouvé sur la page : {} {} {}",
-    //         found_direction, found_title, found_url
-    //     );
-    //     Some(found_url)
-    // }
 
     pub fn get_screenings(&self) -> Screening {
         Screening {
